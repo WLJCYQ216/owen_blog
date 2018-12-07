61033fb73991ce7bb7ba25c1cbf32ebf57405ec2
王立军 <wanglijun@360.net>
2018-12-07 19:03:26 +0800

    route
    update route

--- routes/signin.js
+++ routes/signin.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const router = express.Router()
+const checkNotLogin = require('../middlewares/check').checkNotLogin
+
+router.get('/', checkNotLogin, (req, res, next) => {
+  res.send('登录页')
+})
+
+router.post('/', checkNotLogin, (req, res, next) => {
+  res.send('登录')
+})
+
+module.exports = router
